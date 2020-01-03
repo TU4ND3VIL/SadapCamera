68e6bc00a6bed56819a437aadc650a556b92b944
TU4ND3VIL <59401298+TU4ND3VIL@users.noreply.github.com>
2020-01-03 20:29:54 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+clear
+pkg update
+clear
+pkg upgrade
+clear
+pkg install bash
+clear
+pkg install php
+clear
+pkg install openssh
+clear
+echo "\033[1;32m [\033[1;35m?\033[1;32m] Silahkan Ketik : \033[1;35mbash Moreno77.sh"
+
